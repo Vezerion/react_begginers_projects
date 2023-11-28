01fb2ff7e819c37e85dc01ac6c92e6357cf2f5cf
Ilya <ilia.podrezov2001@gmail.com>
2023-11-28 15:57:58 +0300

    users to complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
@@ -6,10 +6,36 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+  useEffect( () => {
+    async function fetchData() {
+      setIsLoading(true);
+      try {
+        const response = await fetch('https://reqres.in/api/users');
+        if(!response.ok) {
+          throw new Error('Can`t fetch data');
+        }
+        const dataJson = await response.json();
+        setUsers(dataJson.data);
+        setIsLoading(false);
+      } 
+      catch (e) {
+        console.error(e.message)
+      }
+      
+    }
+    fetchData();
+  }, []);
+
+  const onChangeSearchValue = (e) => {
+    setSearchValue(e.target.value);
+  }
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      <Users searchValue={searchValue} onChangeSearchValue={onChangeSearchValue} isLoading={isLoading} items={users}/>
+      <Success />
     </div>
   );
 }
