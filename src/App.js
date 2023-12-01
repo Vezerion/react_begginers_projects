fd3357a88cb9c06f41ffedc87c809f1a51937def
Ilya <ilia.podrezov2001@gmail.com>
2023-12-01 16:48:18 +0300

    currency-convertor completed ver 1.0

--- src/App.js
+++ src/App.js
@@ -4,18 +4,59 @@ import './index.scss';
 
 function App() {
   const [currencies, setCurrencies] = useState([]);
+  const [convertFrom, setConvertFrom] = useState('RUB');
+  const [convertTo, setConvertTo] = useState('USD');
+  const [conversionCoeff, setConversionCoeff] = useState(0);
+  const [fromValue, setFormValue] = useState(0);
+  const [toValue, setToValue] = useState(0);
   useEffect(()=>{
     async function fetchData() {
       const response = await fetch('https://api.freecurrencyapi.com/v1/currencies?apikey=fca_live_P17a5VRf3ZlHA55OTmq7nyWsyiDNrvkUy2AwMq0U&currencies=');
       const dataJson = await response.json()
-      setCurrencies(dataJson.data);
+      setCurrencies(Object.keys(dataJson.data));
     }
     fetchData();
   }, []);
+  useEffect(() => {
+    async function fetchConvertationData() {
+      const response = await fetch(`https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_P17a5VRf3ZlHA55OTmq7nyWsyiDNrvkUy2AwMq0U&currencies=${convertTo}&base_currency=${convertFrom}`);
+      const dataJson = await response.json();
+      setConversionCoeff(dataJson.data[convertTo])
+    }
+    fetchConvertationData()
+  }, [convertFrom, convertTo])
+  function handleSetCurrency(cur, setConvert, setDef) {
+    setConvert(cur);
+    setDef(['RUB', 'USD', 'EUR', cur])
+  }
+  function convertMoney(conversionCoeff, fromValue, setToValue, flag) {
+    setToValue(flag ? (fromValue * conversionCoeff).toFixed(4) : (fromValue / conversionCoeff).toFixed(4))
+  }
+  function handleOnChangeInputValue(e, setFunc) {
+    setFunc(e.target.value);
+  }
   return (
     <div className="App">
-      <Block value={0} currencies={currencies} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currencies={currencies} currency="USD" />
+      <Block 
+      handleSetCurrency={handleSetCurrency} 
+      currencies={currencies} 
+      convert={convertFrom}
+      setConvert={setConvertFrom}
+      value={fromValue}
+      handleOnChangeInputValue={handleOnChangeInputValue}
+      setValue={setFormValue}
+      blur = {() => convertMoney(conversionCoeff, fromValue, setToValue, true)}
+      />
+      <Block 
+      handleSetCurrency={handleSetCurrency} 
+      currencies={currencies}
+      convert={convertTo}
+      setConvert={setConvertTo}
+      value={toValue}
+      handleOnChangeInputValue={handleOnChangeInputValue}
+      setValue={setToValue}
+      blur = {() => convertMoney(conversionCoeff, toValue, setFormValue, false)}
+      />
     </div>
   );
 }
--- src/Block.jsx
+++ src/Block.jsx
@@ -1,31 +1,38 @@
-import React from 'react';
+import { useState } from 'react';
+import { DropDown } from './DropDown';
 
-const defaultCurrencies = ['RUB', 'USD', 'EUR', 'GBP'];
+export const Block = ({ currencies, handleSetCurrency, convert, setConvert, value, handleOnChangeInputValue, setValue, blur  }) => {
+  const [defaultCurrencies, setDefaultCurrencies] = useState(['RUB', 'USD', 'EUR', 'GBP']);
+  // const [value, SetValue] = useState(0);
+  return (
+    <div className="block">
+      <ul className="currencies">
 
-export const Block = ({currencies, value, currency, onChangeValue, onChangeCurrency }) => (
-  <div className="block">
-    {console.log(currencies.EUR)}
-    <ul className="currencies">
-      {defaultCurrencies.map((cur) => (
-        <li
-          onClick={() => onChangeCurrency(cur)}
-          className={currency === cur ? 'active' : ''}
-          key={cur}>
-          {cur}
-        </li>
-      ))}
-      <li>
-        <svg height="50px" viewBox="0 0 50 50" width="50px">
-          <rect fill="none" height="50" width="50" />
-          <polygon points="47.25,15 45.164,12.914 25,33.078 4.836,12.914 2.75,15 25,37.25 " />
-        </svg>
-      </li>
-    </ul>
-    <input
-      onChange={(e) => onChangeValue(e.target.value)}
-      value={value}
-      type="number"
-      placeholder={0}
-    />
-  </div>
-);
+        {defaultCurrencies.map((cur) => (
+          <li
+            onClick={() => setConvert(cur)}
+            className={cur === convert ? 'active' : ''}
+            key={cur}>
+            {cur}
+          </li>
+        ))}
+
+        <DropDown 
+        currencies={currencies}
+        handleSetCurrency={handleSetCurrency} 
+        setDefaultCurrencies={setDefaultCurrencies} 
+        convert={convert}
+        setConvert={setConvert}
+        />
+      </ul>
+      <input
+        // onChange={(e) => SetValue(e.target.value)}
+        onChange={(e) => handleOnChangeInputValue(e, setValue)}
+        onBlur={blur}
+        value={value}
+        type="number"
+        placeholder={0}
+      />
+    </div>
+  )
+};
